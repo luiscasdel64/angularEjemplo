2d9a2098a7d0fc4a9cf11972b6829111dc1d740a
Luis Castañeda <luis.castaneda@utags.edu.mx>
2021-02-16 08:42:11 -0600

    Creación del componente empresas y su servicio.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { EmpresasComponent } from './empresas/empresas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuariosComponent
+    UsuariosComponent,
+    EmpresasComponent
   ],
   imports: [
     BrowserModule,
